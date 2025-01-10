ed1a43aeda5e17ae756155563d651fec961cc6af
Shashank Shekhar <shashankshekharlkrmit1@gmail.com>
2025-01-11 00:23:00 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+    ''' this def will collect the requirements'''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
@@ -17,10 +15,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='MACHINE LEARNING PROJ II',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Shashank Shekhar',
+author_email='shashankshekharlkrmit1@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
