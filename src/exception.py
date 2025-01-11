d72ea30b99b075ce635b6f4b7727398eeb667138
Shashank Shekhar <shashankshekharlkrmit1@gmail.com>
2025-01-11 21:47:36 +0530

    Logger and Exception handler

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-from src.logger import logging
+import logging
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,5 +21,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-
-        
